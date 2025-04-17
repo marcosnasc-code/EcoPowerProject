2bc03b82608581dfa8850aa60b2f7001c1c79f35
marcosnasc-code <marcos.nacimento0321@gmail.com>
2025-04-17 00:39:45 -0300

    Sincronização

--- src/main/java/br/com/fiap/EcoPower/EcoPowerApplication.java
+++ src/main/java/br/com/fiap/EcoPower/EcoPowerApplication.java
@@ -1,15 +1,10 @@
 package br.com.fiap.EcoPower;
 
-import br.com.fiap.EcoPower.model.PermissionRoles;
-import br.com.fiap.EcoPower.model.UsuarioModel;
-import br.com.fiap.EcoPower.repository.UsuarioRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-//Alterar base packages aqui depois
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @EnableMongoRepositories(basePackages = "br.com.fiap.EcoPower.repository")
 @SpringBootApplication
